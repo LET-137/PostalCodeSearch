e36310b75f21b6c8d107377f3cae849092c00f12
LET <bjs.6300.137@gmail.com>
2024-02-02 16:28:56 +0900

    Initial Commit

---
+++ APIPra/APIPraApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIPraApp.swift
+//  APIPra
+//
+//  Created by 津本拓也 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct APIPraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
